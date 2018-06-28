974020d7a2831d56cbd9385b6839be3bd8dfa6a4
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-28 23:54:35 +0530

    'RelationServlet' class under development!!

---
+++ src/main/java/hsl/test/dynamicpagealter/servlets/RelationServlet.java
@@ -0,0 +1,19 @@
+/*  -> Designed for testing and development purposes.
+ *  -> Project to design a DynamicPageAlter webapp prototype.
+ *  -> Development Phase  -- Premature.
+ *  -> Project Type  -- Educational.
+ *  -> Organization -- HSL.
+ *  -> Owner/Code file Designer :
+ *             @ Name - Palash Sarkar.
+ *             @ Email - palashsarkar0007@gmail.com.
+ *  -> Copyright Norms - Every piece of code given below
+ *                       has been written by 'Palash Sarkar (Tj07)'©,
+ *                       and he holds the rights to the file. Not meant to be
+ *                       copied or tampered without prior permission from the author.
+ *  -> Guide -
+ */
+
+package hsl.test.dynamicpagealter.servlets;
+
+public class RelationServlet {
+}
