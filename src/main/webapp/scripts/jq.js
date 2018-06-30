bb011934776781dcc2fe40c8fe11c84d332f038a
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-30 17:50:07 +0530

    Writing js scripts!!

--- src/main/webapp/scripts/jq.js
+++ src/main/webapp/scripts/jq.js
@@ -8,11 +8,31 @@ $(document).ready(function(){
 
         $("span#tab_inp_frm").children("button").click(function(){
 
-            tab_nm = ($("span#tab_inp_frm").children("input").val()).trim();
-            tab_det = ($("span#tab_inp_frm").children("textarea").val()).trim();
+                tab_nm = ($("span#tab_inp_frm").children("input").val()).trim();
+                tab_det = ($("span#tab_inp_frm").children("textarea").val()).trim();
 
-            console.log(tab_nm);
-            console.log(tab_det);
+                $.post("relServlet",
+                        {
+
+                                "tab_nme" : tab_nm,
+                                "tab_type_inf" : tab_det
+
+                        },
+                        function(data,status,xhr)
+                        {
+                                if (xhr.readyState === 4 & xhr.status === 200)
+                                {
+
+                                        alert("Details :<br/>" + xhr.readyState + "<br/>" + status + "<br/>" + xhr.status + "<br/>" + String(data) + "<br/>" + xhr.responseText);
+
+                                }
+                                else
+                                {
+
+                                        alert("Details :<br/>" + xhr.readyState + "<br/>" + status + "<br/>" + xhr.status + "<br/>" + xhr.responseText);
+
+                                }
+                        }, "text");
 
         });
 
