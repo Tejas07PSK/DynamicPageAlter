28269ed687ae6e1c5ff624b8482ff169454b8342
Palash Sarkar <palashsarkar0007@gmail.com>
2018-06-30 13:58:39 +0530

    Writing js scripts!!

---
+++ src/main/webapp/scripts/jq.js
@@ -0,0 +1,11 @@
+/*
+        @Author : Palash Sarkar
+        @CreatedON : 30 JUNE, 2018, 1:46:53 PM
+        @FILEName :  jq.js
+ */
+
+$(document).ready(function(){
+
+        $("span#tab_inp_frm").children("button")
+
+});
